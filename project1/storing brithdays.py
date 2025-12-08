06c51a8c5c8e622613487cc85a70b2fdeace583e
shafiq222 <supermarketazaan@gmail.com>
2025-12-08 12:01:28 +0300

    project1

---
+++ project1/storing brithdays.py
@@ -0,0 +1,14 @@
+Ayaan=3/17/2018
+print("The day when Ayaan was born is:",Ayaan)
+
+Rumman=5/9/2008
+print("The day when Rumman was born is:",Rumman)                                
+
+Me=5/27/2013
+print("The day when I was born is:",Me)
+
+Mydad=4/12/1997
+print("The day when My dad was born is:",Mydad)
+
+Mycousinsis=2/8/2025
+print("The day when My cousin sister was born is:",Mycousinsis)

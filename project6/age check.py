fd163a291cbeabab26e9146e809a137b4e7e70a0
shafiq222 <supermarketazaan@gmail.com>
2025-12-22 11:23:26 +0300

    project6

---
+++ project6/age check.py
@@ -0,0 +1,10 @@
+# Age checking program
+
+age = int(input("Enter your age: "))
+
+if age < 18:
+    print("You are a minor.")
+elif age >= 18 and age < 60:
+    print("You are an adult.")
+else:
+    print("You are a senior citizen.")
